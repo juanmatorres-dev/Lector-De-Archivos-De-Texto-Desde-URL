e43d865c78267dde5e5ba1018578c2bc616e5aef
Juan Manuel <veinte2000jtm23@gmail.com>
2021-08-14 20:06:00 +0200

    Cambio del nombre del fichero

--- src/App/VistaPrincipal.java
+++ src/App/VistaPrincipal.java
@@ -4,7 +4,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 
-public class Vista {
+public class VistaPrincipal {
 
 	private JFrame frame;
 
@@ -15,7 +15,7 @@ public class Vista {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Vista window = new Vista();
+					VistaPrincipal window = new VistaPrincipal();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -27,7 +27,7 @@ public class Vista {
 	/**
 	 * Create the application.
 	 */
-	public Vista() {
+	public VistaPrincipal() {
 		initialize();
 	}
 
