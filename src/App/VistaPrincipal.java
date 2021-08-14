1c35727c754cf26702804f1de4ef2b0aa4d0f9d5
Juan Manuel <veinte2000jtm23@gmail.com>
2021-08-14 20:10:23 +0200

    Añadidas mejoras básicas

--- src/App/VistaPrincipal.java
+++ src/App/VistaPrincipal.java
@@ -6,7 +6,7 @@ import javax.swing.JFrame;
 
 public class VistaPrincipal {
 
-	private JFrame frame;
+	private JFrame ventana_principal;
 
 	/**
 	 * Launch the application.
@@ -16,7 +16,7 @@ public class VistaPrincipal {
 			public void run() {
 				try {
 					VistaPrincipal window = new VistaPrincipal();
-					window.frame.setVisible(true);
+//					window.ventana_principal.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +35,12 @@ public class VistaPrincipal {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		ventana_principal = new JFrame();
+		ventana_principal.setTitle("Lector De Archivos De Texto Desde URL");
+		ventana_principal.setBounds(100, 100, 450, 300);
+		ventana_principal.setLocationRelativeTo(null);
+		ventana_principal.setVisible(true);
+		ventana_principal.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
 }
